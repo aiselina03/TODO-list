8cdaf0ad3cd45344bb3e200689ab406d37f61027
Aysel <146956428+aiselina03@users.noreply.github.com>
2023-11-06 15:15:15 +0400

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,36 @@
 const inputbox = document.querySelector(".inputbox");
 const list = document.querySelector(".list");
 const form = document.querySelector("form");
+const arr = [];
 
-form.onsubmit = function (event) {
-    event.preventDefault();
-    Add();
+form.onsubmit = function (e) {
+  e.preventDefault();
+  Add();
 };
 
 function Add() {
-    if (inputbox.value === "") {
-        alert("You must write something!");
-    } else {
-        let li = document.createElement("li");
-        li.textContent = inputbox.value;
-        list.appendChild(li);
-       
-        const icon1 = document.createElement('i');
-        icon1.className = "fa-solid fa-trash";
-        li.appendChild(icon1);
-        icon1.onclick = () => {
-            li.remove();
-        };
+  if (inputbox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputbox.value;
+    list.appendChild(li);
 
-        const arr = JSON.parse(localStorage.getItem("todo")) || [];
-        arr.push(inputbox.value);
-        localStorage.setItem("todo", JSON.stringify(arr));
+    const icon1 = document.createElement("i");
+    icon1.className = "fa-solid fa-trash";
+    li.appendChild(icon1);
 
-        inputbox.value = '';
+    //local-atmaq
+    arr.push(li.textContent);
+    localStorage.setItem("todo", JSON.stringify(arr)); //local-a yazdirmaq
+    console.log(JSON.parse(localStorage.getItem("todo")));
 
-        console.log(JSON.parse(localStorage.getItem("todo")));
-    }
+    icon1.onclick = () => {
+      li.remove();
+      const liIndex = arr.indexOf(li.textContent);
+      arr.splice(liIndex, 1);
+      localStorage.setItem("todo", JSON.stringify(arr));
+    };
+    inputbox.value = "";
+  }
 }
